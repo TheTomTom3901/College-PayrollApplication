19aa8e621dc631f0d250c7f3832a10e0fa632a2b
Tom <TheTomTom3901@gmail.com>
2018-03-23 20:32:08 +0000

    Uploaded all files

---
+++ Payroll/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Payroll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string directory;
+        int grosspay;
+        double finalpay;
+        int hours;
+        string date;
+        int weeks;
+        string employeename;
+        string department;
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void CalcButton_Click(object sender, EventArgs e)
+        {
+            EmptyError.Hide();
+            hours = (int)HoursWorked.Value;
+            grosspay = hours * 12;
+            GrossPayText.Text = "£" + grosspay.ToString();
+            finalpay = grosspay * 0.72;
+            DeductionsText.Text = "£" + (grosspay - finalpay).ToString();
+            NetPayText.Text = "£" + finalpay.ToString();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            HoursWorked.Value = 1;
+            DepartmentText.SelectedIndex = -1;
+            EmployeeNameText.Text = "Enter name";
+            WeekChooser.Value = 1;
+            DatePicker.ResetText();
+            GrossPayText.Clear();
+            DeductionsText.Clear();
+            NetPayText.Clear();
+        }
+
+        private void ChooseDirectory_Click(object sender, EventArgs e)
+        {
+            if (FolderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                DirectoryText.Text = FolderBrowser.SelectedPath;
+                directory = DirectoryText.Text;
+            }
+        }
+
+        private void GenInvoice_Click(object sender, EventArgs e)
+        {
+            if (EmployeeNameText.Text != "" && DepartmentText.Text != "" && GrossPayText.Text != "" && DeductionsText.Text != "" && NetPayText.Text != "")
+            {
+                EmptyError.Hide();
+                employeename = EmployeeNameText.Text;
+                date = DatePicker.Text;
+                weeks = (int)WeekChooser.Value;
+                department = DepartmentText.Text;
+                hours = (int)HoursWorked.Value;
+                directory = DirectoryText.Text;
+                if(System.IO.Directory.Exists(directory))
+                {
+                    string path = directory + @"\" + employeename + @" Invoice.txt";
+                    if (!System.IO.File.Exists(path))
+                    {
+                        string[] createText = {"iTecCrawley Invoice",
+                                           "--------------------",
+                                           "Name: " + employeename,
+                                           "Department: " + department,
+                                           "--------------------",
+                                           "Date of Pay: " + date.ToString(),
+                                           "Week Number: " + weeks.ToString(),
+                                           "Hours Worked: " + hours.ToString(),
+                                           "--------------------",
+                                           "Gross Pay: " + GrossPayText.Text,
+                                           "Deductions: " + DeductionsText.Text,
+                                           "Net Pay: " + NetPayText.Text,
+                                           "--------------------"};
+                        System.IO.File.WriteAllLines(path, createText);
+                        MessageBox.Show("An invoice has been created for " + employeename);
+                    }
+                    else
+                    {
+                        MessageBox.Show("An invoice for " + employeename + " already exists");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("That directory does not exist!");
+                }
+            }
+            else
+            {
+                EmptyError.Show();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            EmployeeNameText.Select();
+        }
+
+        private void LogoutButton_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Hide();
+        }
+
+        private void InvoicesButton_Click(object sender, EventArgs e)
+        {
+            directory = DirectoryText.Text;
+            if (System.IO.Directory.Exists(directory))
+            {
+                Process.Start(directory);
+            }
+            else
+            {
+                MessageBox.Show("That directory does not exist!");
+            }
+        }
+
+        private void EmployeeNameText_Click(object sender, EventArgs e)
+        {
+            EmployeeNameText.Text = "";
+            EmptyError.Hide();
+        }
+
+        private void DepartmentText_Click(object sender, EventArgs e)
+        {
+            EmptyError.Hide();
+        }
+
+        private void HoursWorked_Click(object sender, EventArgs e)
+        {
+            EmptyError.Hide();
+        }
+
+        private void WeekChooser_Click(object sender, EventArgs e)
+        {
+            EmptyError.Hide();
+        }
+    }
+}
---
+++ Payroll/Login.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Payroll
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            if(UsernameText.Text == "iTecCrawley" && PasswordText.Text == "admin")
+            {
+                Form1 form = new Payroll.Form1();
+                form.Show();
+                this.Hide();
+            }
+            else
+            {
+                ErrorLabel.Show();
+            }
+        }
+
+        private void PasswordText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                LoginButton.PerformClick();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void UsernameText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                LoginButton.PerformClick();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            UsernameText.Select();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void PasswordText_Click(object sender, EventArgs e)
+        {
+            ErrorLabel.Hide();
+        }
+
+        private void UsernameText_Click(object sender, EventArgs e)
+        {
+            ErrorLabel.Hide();
+        }
+
+        private void UsernameText_TextChanged(object sender, EventArgs e)
+        {
+            ErrorLabel.Hide();
+        }
+
+        private void PasswordText_TextChanged(object sender, EventArgs e)
+        {
+            ErrorLabel.Hide();
+        }
+    }
+}
